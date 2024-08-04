8e13cd982884d4b4f88f3d1b5502b0e7d4d4847a
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-05 00:16:10 +0330

    Update app.js
    It will add to the number of rounds.

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const app = Vue.createApp({
       this.attackPlayer();
     },
     healPlayer() {
+      this.currentRound++;
       const healValue = getRandomValue(8, 20);
       if (this.playerHealth + healValue > 100) {
         this.playerHealth = 100;
