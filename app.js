34a3efb21515d9677889732691a7b65fc4ff812a
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-05 23:37:55 +0330

    Create new watchers
    They set the health bar to exactly zero percent when it gets less than zero; otherwise, they would simply return the health value.

--- app.js
+++ app.js
@@ -24,6 +24,18 @@ const app = Vue.createApp({
   },
 
   watch: {
+    monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.playerHealth + "%" };
+    },
     playerHealth(value) {
       if (value <= 0 && this.monsterHealth <= 0) {
         // A Draw
